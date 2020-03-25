d8ba7ea6a40aa41bcc3be3a0aea8cf3a27e5d642
mikejunt <mikejunt@gmail.com>
2020-03-24 22:48:19 -0500

    refactors pitching

--- ClientApp/src/app/services/pitching-query.service.ts
+++ ClientApp/src/app/services/pitching-query.service.ts
@@ -1,37 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Store } from '@ngrx/store';
 import * as Actions from '../store/actions/'
 import { AppState } from '../store';
 import { SearchTerms } from '../interfaces/search.terms.interface';
+import { Pitcher } from '../interfaces/pitcher.interface';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class PitchingService {
-  lgFIPconstant: number = 3.10
-
+  private pitchUrl: string = "https://baseball-api.azurewebsites.net/api/pitching?"
   constructor(private http: HttpClient, private store: Store<AppState>) { }
 
-  collect(players, stat: string): number {
-    return players.reduce((acc: number, player: Object) => acc + parseFloat(player[stat]), 0)
-  }
-
   fetchSeasonPitching(terms: SearchTerms) {
-    if (terms.teamfilter === "allteams") {
-      this.http.post('/api/pitching/all', { season: terms.searchyear, minip: terms.ipfilter }).subscribe(res => {
-        if (!res['success']) { console.log(res) }
-        else { this.store.dispatch(Actions.savePitchers({ pitching: res['data'] })) }
-      })
+    const params = new HttpParams()
+    .set('season', terms.searchyear)
+    .set('team', terms.teamfilter)
+    .set('ip', terms.ipfilter)
+    this.http.get(this.pitchUrl,{params}).subscribe((res:Pitcher[])=> {
+       this.store.dispatch(Actions.savePitchers({ pitching: res }))})
     }
-    else {
-      this.http.post(`/api/pitching/${terms.teamfilter}`, { season: terms.searchyear, minip: terms.ipfilter }).subscribe(res => {
-        if (!res['success']) { console.log(res) }
-        else { this.store.dispatch(Actions.savePitchers({ pitching: res['data'] })) }
-      })
-    }
-  }
 
   logError(err) { console.log(err); return err }
 
