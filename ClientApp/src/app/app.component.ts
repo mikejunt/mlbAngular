5d2e54ee6b3d0ac807fc5a8e842ad9ec9bf188e1
mikejunt <mikejunt@gmail.com>
2020-03-25 16:52:02 -0500

    reverts auth0 approach

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { AppState } from './store';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import * as Selectors from './store/selectors'
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,7 @@ export class AppComponent {
   title = "Mike's Baseball App";
   username$: Observable<string>
   favteam$: Observable<string>
-  constructor (private store: Store<AppState>) {
+  constructor (private store: Store<AppState>, private auth: AuthService) {
     this.username$ = this.store.select(Selectors.viewUserName)
     this.favteam$ = this.store.select(Selectors.viewUserFav)
   }
--- ClientApp/src/app/services/auth.service.ts
+++ ClientApp/src/app/services/auth.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { secret } from '../client-secret';
 // import createAuth0Client  from '@auth0/auth0-spa-js';
 // import Auth0Client from '@auth0/auth0-spa-js/dist/typings/Auth0Client';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
@@ -13,25 +12,10 @@ import * as Actions from '../store/actions';
   providedIn: 'root'
 })
 export class AuthService {
-  auth0url: string = 'https://dev-6-503zbg.auth0.com/oauth/token';
-  apiAuth0$: Observable<Object>
-
 
   constructor(private router: Router, private http: HttpClient, private store: Store<AppState>) {
-    console.log("Hello I am the auth service constructor.")
-    this.apiAuth0$ = this.getApiAuthorization()
-    this.apiAuth0$.subscribe(res => {console.log(res)})
+
    }
 
-   getApiAuthorization(): Observable<Object> {
-    let cli_sec = secret;
-    let apibody = {form: {
-                  grant_type: 'client_credentials',
-                  client_id: 'E57t51xdd4F19G3xv65ZYf6plda5tBL4',
-                  client_secret: cli_sec,
-                  audience: 'https://mybaseballapp.com'
-    }}
-    const apiHeader = new HttpHeaders().set('content-type', 'application/x-www-form-urlencoded')
-    return this.http.post(this.auth0url,apibody,{headers: apiHeader})
-   }
+
 }
