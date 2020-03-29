98ca1716ebdfe66cb6585ed0eb47d5fe0733d720
mikejunt <mikejunt@gmail.com>
2020-03-29 13:11:30 -0500

    latest attempt at the service worker

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { AppState } from './store';
 import { Store } from '@ngrx/store';
-import * as Selectors from './store/selectors';
 import { AuthService } from './services/auth.service';
 import { StaticqueryService } from './services/static-query.service';
 import { RosterService } from './services/roster-query.service';
 import { PitchingService } from './services/pitching-query.service';
 import { HittingService } from './services/hitting-query.service';
-import { HttpParams } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { UserService } from './services/user.service';
 
@@ -19,20 +17,10 @@ import { UserService } from './services/user.service';
 })
 export class AppComponent implements AfterViewInit {
   title = "Mike's Baseball App";
-  team$ = this.store.select(Selectors.viewUserFav)
 
   constructor (private store: Store<AppState>, private auth: AuthService, private user: UserService, private staticquery:
     StaticqueryService, private roster: RosterService, private pitching: PitchingService, 
     private hitting: HittingService, private router: Router) {
-    this.team$.subscribe(res=> this.getinitialData(res))
-  }
-
-  getinitialData(teamid: string) {
-    this.staticquery.fetchTeamDetails(teamid)
-    const params = new HttpParams().set('team_id', `'${teamid}'`)
-    this.roster.fetchRoster(params)
-    this.pitching.fetchSeasonPitching({searchyear: "2019", teamfilter: teamid, posfilter: "", ipfilter: "50"})
-    this.hitting.fetchSeasonHitting({searchyear: "2019", teamfilter: teamid, posfilter: "", pafilter: "300"})
   }
 
   ngAfterViewInit(){
--- ClientApp/src/app/services/user.service.ts
+++ ClientApp/src/app/services/user.service.ts
@@ -5,6 +5,11 @@ import { AppState } from '../store';
 import * as Actions from '../store/actions';
 import * as Selectors from '../store/selectors'
 import { AuthService } from './auth.service';
+import { StaticqueryService } from './static-query.service';
+import { RosterService } from './roster-query.service';
+import { PitchingService } from './pitching-query.service';
+import { HittingService } from './hitting-query.service';
+import { HttpParams } from '@angular/common/http';
 
 
 @Injectable({
@@ -15,7 +20,9 @@ export class UserService {
   favteam: string;
   user$: Observable<Object>
 
-  constructor(private store: Store<AppState>, private auth: AuthService) {
+  constructor(private store: Store<AppState>, private auth: AuthService, private staticquery:
+    StaticqueryService, private roster: RosterService, private pitching: PitchingService, 
+    private hitting: HittingService) {
   }
 
   setUserData(){
@@ -24,10 +31,17 @@ export class UserService {
       this.store.dispatch(Actions.login({user: {username: res['nickname'], favteam: res['http://msj-baseball-favteam']}}))
       this.store.dispatch(Actions.setViewTeam({displayteam: res['http://msj-baseball-favteam']}))})
     this.favteam$ = this.store.select(Selectors.viewUserFav);
-    this.favteam$.subscribe(res => { this.changeFavTheme(res); this.favteam = res })
+    this.favteam$.subscribe(res => { this.changeFavTheme(res); this.favteam = res, this.getinitialData(res) })
 
   }
 
+  getinitialData(teamid: string) {
+    this.staticquery.fetchTeamDetails(teamid)
+    const params = new HttpParams().set('team_id', `'${teamid}'`)
+    this.roster.fetchRoster(params)
+    this.pitching.fetchSeasonPitching({searchyear: "2019", teamfilter: teamid, posfilter: "", ipfilter: "50"})
+    this.hitting.fetchSeasonHitting({searchyear: "2019", teamfilter: teamid, posfilter: "", pafilter: "300"})
+  }
 
   changeFavTheme(newfav: string) {
     let body = document.getElementById("body")
